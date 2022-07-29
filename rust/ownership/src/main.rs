6a96f779d5183958a2f2e1b9625f6ce803f06a91
silkrow <si1krow@outlook.com>
2022-07-29 21:54:03 +0800

    .

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -4,4 +4,13 @@ fn main() {
 	s.push_str(", world!");
 
     println!("{}", s);
+
+	let _x = 5;
+	let _y = _x;
+
+	let s1 = String::from("hello");
+	let s2 = s1.clone();
+
+	println!("{}, world!", s1);
+	println!("{}, world!", s2);
 }
---
+++ rust/ownership/target/debug/deps/ownership-0b9f812ff038886e.d
@@ -0,0 +1,5 @@
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/ownership/target/debug/deps/ownership-0b9f812ff038886e.rmeta: src/main.rs
+
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/ownership/target/debug/deps/ownership-0b9f812ff038886e.d: src/main.rs
+
+src/main.rs:
