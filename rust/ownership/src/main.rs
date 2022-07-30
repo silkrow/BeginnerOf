f43a36d899af6710d6b6cb8734afb63380f0c381
silkrow <si1krow@outlook.com>
2022-07-30 22:00:06 +0800

    added codes

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -13,4 +13,39 @@ fn main() {
 
 	println!("{}, world!", s1);
 	println!("{}, world!", s2);
+
+	let s = String::from("hello");
+
+	let s = moves_ownership(s);
+	let s = s;
+
+	takes_ownership(s); // s's value moves into the function ... and so is no longer valid here.
+
+	println!("{}, world!", s1);
+	let s1 = String::from("hello");
+	
+	let (s, len) = calculate_length(s1);
+
+	println!("The length of '{}' is {}.", s, len);
+
+	let len = cal_length(&s);
+	println!("The length of '{}' is {}.", s, len);
+}
+
+fn takes_ownership(some_string: String) {
+	println!("{}", some_string);
+}
+
+fn moves_ownership(some_string: String) -> String {
+	some_string
+}
+
+fn calculate_length(s: String) -> (String, usize){
+	let length = s.len();
+
+	(s, length)
+}
+
+fn cal_length(s: &String) -> usize {
+	s.len()
 }
