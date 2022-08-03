e9e7fc9c2e973078eee81c77e08c39763480a8db
silkrow <si1krow@outlook.com>
2022-08-03 18:25:09 +0800

    started enum

---
+++ rust/enumeration/src/main.rs
@@ -0,0 +1,9 @@
+enum IpAddrKind {
+	v4,
+	v6,
+}
+
+fn main() {
+	let four = IpAddrKind::v4;
+	let six = IpAddrKind::v6;
+}
---
+++ rust/enumeration/target/debug/deps/enumeration-bc7892dc3252ded2.d
@@ -0,0 +1,5 @@
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/enumeration/target/debug/deps/enumeration-bc7892dc3252ded2.rmeta: src/main.rs
+
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/enumeration/target/debug/deps/enumeration-bc7892dc3252ded2.d: src/main.rs
+
+src/main.rs:
