52eebd05037b858d53a827befb7420f370ec3ffe
silkrow <si1krow@outlook.com>
2022-08-04 23:07:42 +0800

    .

--- rust/enumeration/src/main.rs
+++ rust/enumeration/src/main.rs
@@ -40,7 +40,10 @@ enum Coin {
 
 fn in_cents(coin: Coin) -> u8 {
 	match coin {
-		Coin::Penny => 1,
+		Coin::Penny => {
+			println!("Lucky Penny!");
+			1
+		},
 		Coin::Nickel => 5,
 		Coin::Dime => 10,
 		Coin::Quarter => 25,
