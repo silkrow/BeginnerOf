401dfaf401c8dfd133a5b420867d9dad1c4b3f2f
silkrow <si1krow@outlook.com>
2022-08-09 10:53:05 +0800

    .

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -103,4 +103,15 @@ fn main() {
 	for b in "नमसत्".bytes() {
 		println!("{}", b);
 	}
+
+	use std::collections::HashMap;
+	let mut scores = HashMap::new();
+
+	scores.insert(String::from("Blue"), 10);
+	scores.insert(String::from("Yellow"), 50);
+
+	let teams = vec![String::from("Blue"), String::from("Yellow")];
+	let initial_scores = vec![10, 50];
+
+	let _scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 }
