71c8f30d79d4c1155d11a2eef212c6f54ba59e08
silkrow <si1krow@outlook.com>
2022-08-14 22:48:49 -0500

    .

---
+++ rust/generics/target/debug/deps/generics-a569b1ff32275d9f.d
@@ -0,0 +1,5 @@
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/generics/target/debug/deps/generics-a569b1ff32275d9f: src/main.rs
+
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/generics/target/debug/deps/generics-a569b1ff32275d9f.d: src/main.rs
+
+src/main.rs:
---
+++ rust/generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/generics/target/debug/generics: /Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/generics/src/main.rs
