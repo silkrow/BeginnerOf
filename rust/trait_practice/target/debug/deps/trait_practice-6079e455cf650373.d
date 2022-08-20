ce1716bebe28535dd219e000c5232768a55c74ac
silkrow <si1krow@outlook.com>
2022-08-20 17:09:37 -0500

    .

---
+++ rust/trait_practice/target/debug/deps/trait_practice-6079e455cf650373.d
@@ -0,0 +1,5 @@
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/trait_practice/target/debug/deps/trait_practice-6079e455cf650373.rmeta: src/main.rs
+
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/trait_practice/target/debug/deps/trait_practice-6079e455cf650373.d: src/main.rs
+
+src/main.rs:
---
+++ rust/trait_practice/target/debug/deps/trait_practice-e1b6d89a52ab1615.d
@@ -0,0 +1,5 @@
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/trait_practice/target/debug/deps/trait_practice-e1b6d89a52ab1615.rmeta: src/lib.rs
+
+/Users/yuerkai/Desktop/fromgit/BeginnerOf/rust/trait_practice/target/debug/deps/trait_practice-e1b6d89a52ab1615.d: src/lib.rs
+
+src/lib.rs:
